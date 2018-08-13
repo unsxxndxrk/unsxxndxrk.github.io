30c2daff5d2f5b8852fea3a53f147f02ff183144
Igor <unsxxndxrk@users.noreply.github.com>
2018-08-13 21:08:41 +0300

    test

--- js/main.js
+++ js/main.js
@@ -51,4 +51,21 @@ $(document).ready(function() {
 	  arrows: false,
 	  autoplay: true
 	});
+
+    //E-mail Ajax Send
+    $("form").submit(function() { //Change
+        var th = $(this);
+        $.ajax({
+            type: "POST",
+            url: "mail.php", //Change
+            data: th.serialize()
+        }).done(function() {
+            alert("Thank you!");
+            setTimeout(function() {
+                // Done Functions
+                th.trigger("reset");
+            }, 1000);
+        });
+        return false;
+    });
 });
