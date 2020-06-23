6a05833e4c2400b1f03f9922bd535a260b2d84bf
unsxxndxrk <selincorvin@yahoo.com>
2020-06-23 18:48:36 +0300

    added Weather App (ReactJS)

---
+++ precache-manifest.7b9e7ddba85d1ea17ba0ee086ee2d04f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "303b0e60de6ad5714457",
+    "url": "/static/css/main.a53d7007.chunk.css"
+  },
+  {
+    "revision": "303b0e60de6ad5714457",
+    "url": "/static/js/main.303b0e60.chunk.js"
+  },
+  {
+    "revision": "b4213185c9f6af0ce3f3",
+    "url": "/static/css/1.16406e63.chunk.css"
+  },
+  {
+    "revision": "b4213185c9f6af0ce3f3",
+    "url": "/static/js/1.b4213185.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "61334e9c7eaecf65c419b357630f00dc",
+    "url": "/index.html"
+  }
+];
